722943a05cd30b095c2a9d7285e5c5086478f5e5
unknown <aayushd@iitbhilai.ac.in>
2022-06-25 15:06:57 +0530

    Dao init

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a dao for holders of Crypto Devs NFTs.
+
+Members can create Proposal and vote for new NFTs to buy or not.
---
+++ hardhat-contracts/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xBC4F021eb316411D9555dA97a5CeCC8d89667827";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
